cf1ed995ed25e0248a43033ccfac1ed878f01702
Usama Ali <aliusama481@yahoo.com>
2025-05-17 11:26:21 +0200

    [ADD] * vban typesbundle

--- packages/apps-config/src/api/typesBundle.ts
+++ packages/apps-config/src/api/typesBundle.ts
@@ -11,6 +11,59 @@ import type { OverrideBundleType } from '@polkadot/types/types';
 
 export const typesBundle = {
   "chain": {
+      "node-vb": {
+          "instances": {
+              "council": [
+                  "councilCollective"
+              ],
+              "technicalCommittee": [
+                  "techCommitteeCollective"
+              ]
+          },
+          "types": [
+              {
+                  "minmax": [
+                      0,
+                      null
+                  ],
+                  "types": {
+                      "AccountId": "EthereumAccountId",
+                      "AccountId20": "EthereumAccountId",
+                      "AccountId32": "EthereumAccountId",
+                      "Address": "AccountId20",
+                      "Keys": "SessionKeys3",
+                      "LookupSource": "AccountId20",
+                      "Balance": "u128",
+                      "Nominator": {
+                          "nominations": "Vec<Bond>",
+                          "total": "Balance"
+                      },
+                      "Bond": {
+                          "owner": "EthereumAccountId",
+                          "amount": "Balance"
+                      },
+                      "ValidatorStatus": {
+                          "_enum": {
+                              "Active": "Null",
+                              "Idle": "Null",
+                              "Leaving": "RoundIndex"
+                          }
+                      },
+                      "EthereumSignature": {
+                          "r": "H256",
+                          "s": "H256",
+                          "v": "U8"
+                      },
+                      "Account": {
+                          "nonce": "U256",
+                          "balance": "u128"
+                      },
+                      "EthTransaction": "LegacyTransaction",
+                      "ExtrinsicSignature": "EthereumSignature"
+                  }
+              }
+          ]
+      },
     "Cere Mainnet Beta": {
       "types": [
         {
